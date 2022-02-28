6c256f6887760268a0f87333885e15986f51c0ea
Timothy van den Beitel <2157002@talnet.nl>
2022-02-28 15:06:46 +0100

    insert 1

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,17 @@
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES ('1','Dirk','Icker','9B','Albert Cuypstraat 1','1234AB','2003-2-12','Amsterdam');
 
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES ('5','Bryan','Thomas','9C','Nijl 5','1186JC','2002-4-8','Amstelveen');
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES ('6','Rick','Baard','9C','Albert Cuypstraat 8','1234AB','2001-6-02','Amsterdam');
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES ('7','Luuk','Scharn','9B','Pieter Aertszstraat','1073CD','2002-9-22','Amstelveen');
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES ('8','Danique','Berg','9B','Nijl 125','1186JC','2002-1-16','Amstelveen');
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES ('9','Angela','Mohammed','9C','Piet Mondriaanplein 14','1299CD','2003-5-20','Amstelveen');
