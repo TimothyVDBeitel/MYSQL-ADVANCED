998cc179d90a7a9a2c550235cb6e0899e6ed630d
Timothy van den Beitel <2157002@talnet.nl>
2022-03-01 09:50:45 +0100

    update 1

--- 2-UPDATE/taak01 - Update/antwoorden.sql
+++ 2-UPDATE/taak01 - Update/antwoorden.sql
@@ -1,9 +1,13 @@
 -- Opdracht 1
-
+UPDATE `studenten2` SET `woonplaats`='Amstelveen'
 -- Opdracht 2 
-
+UPDATE `studenten2` SET `adres`='Nijl 25',`postcode`='1186JC' WHERE student_id= 8
+UPDATE `studenten2` SET `adres`='Pieter Aertszstraat 20',`postcode`='1073CD' WHERE student_id= 9
+UPDATE `studenten2` SET `adres`='Nijl 100',`postcode`='1186JC' WHERE student_id= 10
 -- Opdracht 3
-
+UPDATE `studenten2` SET `geboortedatum`='2000-12-12' WHERE student_id= 2
 -- Opdracht 4
-
+UPDATE `studenten2` SET`klas`='9C'WHERE student_id= 4
+UPDATE `studenten2` SET`klas`='9A'WHERE student_id= 8
 -- Opdracht 5
+UPDATE `studenten2` SET`voornaam`='Johannah' WHERE student_id= 6
