a6a88b6119373e2aeb94c063484ad47868da9b6e
Timothy van den Beitel <2157002@talnet.nl>
2022-03-01 09:22:47 +0100

    insert 2

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,8 @@
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, geboortedatum, woonplaats)
+VALUES ('10', 'Mike', 'Verburg', '9C', '2001-3-15', 'Amsterdam')
 
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, geboortedatum, woonplaats)
+VALUES ('11', 'Peter', 'Parker', '9C', '2000-11-09', 'Aalsmeer')
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, geboortedatum, woonplaats)
+VALUES ('12', 'Luke', 'Skywalker', '9C', '1999-6-25', 'Uithoorn')
